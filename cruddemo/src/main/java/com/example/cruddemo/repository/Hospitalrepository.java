bb370f3be52b37168f5a82012b8d98b626be62ca
Prajusha <113427930+prajusha0930@users.noreply.github.com>
2023-04-29 11:40:03 +0530

    Add files via upload

---
+++ cruddemo/src/main/java/com/example/cruddemo/repository/Hospitalrepository.java
@@ -0,0 +1,29 @@
+package com.example.cruddemo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Modifying;
+//import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.cruddemo.model.Hospital;
+
+@Repository
+public interface Hospitalrepository extends JpaRepository<Hospital,Integer> {
+    //positional parameter
+	/*@Query("select s from Hospital s where s.gname=?1 and s.name=?2")
+	public List<Hospital>getHospitalByName(String gname,String name);*/
+	//named parameter
+	@Query("select s from Hospital s where s.name=:name")
+	public List<Hospital>getHospitalByName(String name);
+	//
+		/*@Modifying
+		@Query("delete from Hospital s where s.name=?1")
+		public int deleteHosByName(String name);*/
+	
+List<Hospital> findByNameStartingWith(String prefix);
+List<Hospital> findByNameEndingWith(String suffix);
+List<Hospital> findByGname(String gname);
+}
